308287bf7083888e475587e2f49e07b2d04028f7
gabor182 <schmied.gabor@gmail.com>
2018-09-17 08:53:28 +0200

    checking user roles before uploading a new photo

--- Starter Files/ImageGallery/src/ImageGallery.Client/Controllers/GalleryController.cs
+++ Starter Files/ImageGallery/src/ImageGallery.Client/Controllers/GalleryController.cs
@@ -124,7 +124,8 @@ namespace ImageGallery.Client.Controllers
        
             throw new Exception($"A problem happened while calling the API: {response.ReasonPhrase}");
         }
-        
+
+        [Authorize(Roles = "PayingUser")]
         public IActionResult AddImage()
         {
             return View();
@@ -132,6 +133,7 @@ namespace ImageGallery.Client.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "PayingUser")]
         public async Task<IActionResult> AddImage(AddImageViewModel addImageViewModel)
         {   
             if (!ModelState.IsValid)
